0da4d9684dcea2ad385051259d8b41956f9d88bc
cagricagman <cagri.cagman@gmail.com>
2022-09-01 00:31:46 +0300

    Katmanlı Mimari için proje oluşturuldu. Brand ile ilgili entity tarafında, repository kısmında gerekli eklemeler yapıldı. Veritabanında ilgili kısımlar oluşturuldu.

---
+++ src/demoProjects/AracKiralama/Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistence.Contexts;
+
+public class BaseDbContext : DbContext
+{
+    public IConfiguration Configuration { get; set; }
+    public DbSet<Brand> Brands { get; set; }
+
+    public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Brand>(a =>
+        {
+            a.ToTable("Brands").HasKey(k => k.Id);
+            a.Property(p => p.Id).HasColumnName("Id");
+            a.Property(p => p.Name).HasColumnName("Name");
+        });
+
+
+
+        Brand[] brandEntitySeeds = { new(1, "Renault"), new(2, "Bmw"), new(3, "Mercedes") };
+        modelBuilder.Entity<Brand>().HasData(brandEntitySeeds);
+
+
+    }
+}
