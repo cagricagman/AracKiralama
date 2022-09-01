cb27ee4cf541a8e6afd62c2337fd91a5c7740d91
cagricagman <cagri.cagman@gmail.com>
2022-09-01 23:11:59 +0300

    Application katmanında brand ile ilgili CQRS işlemleri dahil edildi. Exception ile ilgili middleware işlemi dahil edildi. BusinessRole yazıldı.

---
+++ src/demoProjects/AracKiralama/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
@@ -0,0 +1,38 @@
+﻿using Application.Features.Brands.Dtos;
+using Application.Features.Brands.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Brands.Commands.CreateBrand;
+
+public class CreateBrandCommand : IRequest<CreatedBrandDto>
+{
+    public string Name { get; set; }
+
+    public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandDto>
+    {
+        private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
+        private readonly BrandBusinessRules _brandBusinessRules;
+
+        public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper, BrandBusinessRules brandBusinessRules)
+        {
+            _brandRepository = brandRepository;
+            _mapper = mapper;
+            _brandBusinessRules = brandBusinessRules;
+        }
+
+        public async Task<CreatedBrandDto> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            await _brandBusinessRules.BrandNameCanNotBeDuplicatedWhenInserted(request.Name); // business tarafında gerekli kontroller yapılarak create işlemi gerçekleştirme aşamasına geçer
+
+            Brand mappedBrand = _mapper.Map<Brand>(request); //Gelen request i brand clasına map işlemi gerçekleştirdi
+            Brand createdBrand = await _brandRepository.AddAsync(mappedBrand); // ekleme işlemi yapıldı
+            CreatedBrandDto createdBrandDto = _mapper.Map<CreatedBrandDto>(createdBrand); // veritabanına eklenen kaydın tüm bilgilerinden ilgili alanlarının geri dönüşünü sağlayarak dto doldurmasına imkan sağlar
+            
+            return createdBrandDto;
+        }
+    }
+}
---
+++ src/demoProjects/AracKiralama/Application/Features/Brands/Rules/BrandBusinessRules.cs
@@ -0,0 +1,22 @@
+﻿using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+using Core.Persistence.Paging;
+using Domain.Entities;
+
+namespace Application.Features.Brands.Rules;
+
+public class BrandBusinessRules
+{
+    private readonly IBrandRepository _brandRepository;
+
+    public BrandBusinessRules(IBrandRepository brandRepository)
+    {
+        _brandRepository = brandRepository;
+    }
+
+    public async Task BrandNameCanNotBeDuplicatedWhenInserted(string name)
+    {
+        IPaginate<Brand> results = await _brandRepository.GetListAsync(b => b.Name == name);
+        if (results.Items.Any()) throw new BusinessException("Brand Name Exists.");
+    }
+}
