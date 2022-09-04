6e4a486b9023a7dc8799532507aa53d200d25290
cagricagman <cagri.cagman@gmail.com>
2022-09-04 18:41:14 +0300

    Brand için list query işlemleri eklendi. fluent validation tool aktif edildi. profil kısmında eklemeler yapıldı. application service kısmında ilgili toolar aktif edildi.

---
+++ src/demoProjects/AracKiralama/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Brands.Commands.CreateBrand;
+
+public class CreateBrandCommandValidator : AbstractValidator<CreateBrandCommand>
+{
+    public CreateBrandCommandValidator()
+    {
+        RuleFor(c => c.Name).NotEmpty().WithMessage("İsim Alanı Boş Geçilemez");
+        RuleFor(c => c.Name).MinimumLength(3).WithMessage("Min. 3 Karakter Giriniz");
+    }
+}
